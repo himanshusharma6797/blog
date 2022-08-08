b95573c0a0fb4dc36af452eab9d6ebc05f18c45e
himanshusharma6797 <himanshusharma06071997@gmail.com>
2022-08-08 16:06:47 +0530

    8 august JS

---
+++ Daily Practice/JS/currying.js
@@ -0,0 +1,14 @@
+const multiArgFunction = (a, b, c) => a + b + c;
+console.log(multiArgFunction(1, 2, 3)); // 6
+
+const curryUnaryFunction = (a) => {return (b) =>{return (c) =>{return a + b + c}}};
+
+console.log(curryUnaryFunction(1));
+// returns a function: b => c =>  1 + b + c
+// output will be
+
+console.log(curryUnaryFunction(1)(2)); 
+// returns a function: c => 3 + c
+
+console.log(curryUnaryFunction(1)(2)(3));
+// returns the number 6
